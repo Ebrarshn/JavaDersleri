e1104ab50df2b6e1e2e93234f0c4c76b98f5b5e2
Ebrarshn <159464276+Ebrarshn@users.noreply.github.com>
2024-07-05 11:11:07 +0300

    Mükemmel sayı

--- JavaDersleri/src/enesbayramdersleri/mukemmelSayi.java
+++ JavaDersleri/src/enesbayramdersleri/mukemmelSayi.java
@@ -4,22 +4,22 @@ public class mukemmelSayi {
 
 	public static void main(String[] args) {
 		Scanner inp=new Scanner(System.in);
-	
-		int total=0;
 		System.out.print("Bir sayı giriniz: ");
-		int number=inp.nextInt();
-		for(int i=1; i<number; i++) {
-			
-			if(number%i==0) {
+		int n=inp.nextInt();
+		int total=0;
+		
+			for(int i=1; i<=n-1; i++) {
+				if(n%i==0) {
 				System.out.println(i);
-				 total+=i;
+				total+=i;
+				}
+				
 			}
-		       }
-		if(number==total) {
-			System.out.println(number + " bir mükemmel sayıdır...");
-		}else {
-			System.out.println(number + " bir mükemmel sayı değildir...");
-		}
+			if(n==total) {
+				System.out.println(n + " mükemmel bir sayıdır");
+			}else {
+				System.out.println(n + " mükemmel bir sayı değildir");	
+			}
 	}
 
 }
